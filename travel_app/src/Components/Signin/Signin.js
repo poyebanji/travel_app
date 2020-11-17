c1ea7c051ff7ebb4026739bd50c3c525e4412d89
E4ma <ifeomaidoko@gmail.com>
2020-11-16 22:14:01 -0700

    Signin update

--- travel_app/src/Components/Signin/Signin.js
+++ travel_app/src/Components/Signin/Signin.js
@@ -1,20 +1,94 @@
-import React, {Component} from 'react';
-import {Container,Button} from 'react-bootstrap'
+import React, { useState } from "react";
+import "./../Signin/Signin.css";
+import { useSpring, animated } from "react-spring";
 
-class Signin extends Component {
-    state = {  }
-    render() { 
-        return (
-            <Container>
-                <h2>I am Signin Page Update 2020-11-16 </h2>
-            <Button type='button' size='lg' block>Signup</Button>
-        
-            <Button type='button'>Login</Button>
-                
-            </Container>
-          );
+function Signin() {
+    const [registrationFormStatus, setRegistartionFormStatus] = useState(false);
+    const loginProps = useSpring({ 
+      left: registrationFormStatus ? -500 : 0, // Login form sliding positions
+    });
+    const registerProps = useSpring({
+      left: registrationFormStatus ? 0 : 500, // Register form sliding positions 
+    });
+  
+    const loginBtnProps = useSpring({
+      borderBottom: registrationFormStatus 
+        ? "solid 0px transparent"
+        : "solid 2px #1059FF",  //Animate bottom border of login button
+    });
+    const registerBtnProps = useSpring({
+      borderBottom: registrationFormStatus
+        ? "solid 2px #1059FF"
+        : "solid 0px transparent", //Animate bottom border of register button
+    });
+  
+    function registerClicked() {
+      setRegistartionFormStatus(true);
     }
-}
- 
-export default Signin;
+    function loginClicked() {
+      setRegistartionFormStatus(false);
+    }
+  
+    return (
+      <div className="login-register-wrapper">
+        <div className="nav-buttons">
+          <animated.button
+            onClick={loginClicked}
+            id="loginBtn"
+            style={loginBtnProps}
+          >
+            Login
+          </animated.button>
+          <animated.button
+            onClick={registerClicked}
+            id="registerBtn"
+            style={registerBtnProps}
+          >
+            Register
+          </animated.button>
+        </div>
+        <div className="form-group">
+          <animated.form action="" id="loginform" style={loginProps}>
+            <LoginForm />
+          </animated.form>
+          <animated.form action="" id="registerform" style={registerProps}>
+            <RegisterForm />
+          </animated.form>
+        </div>
+        <animated.div className="forgot-panel" style={loginProps}>
+          <a herf="#">Forgot your password</a>
+        </animated.div>
+      </div>
+    );
+  }
+  
+  function LoginForm() {
+    return (
+      <React.Fragment>
+        <label for="username">USERNAME</label>
+        <input type="text" id="username" />
+        <label for="password">PASSWORD</label>
+        <input type="text" id="password" />
+        <input type="submit" value="submit" className="submit" />
+      </React.Fragment>
+    );
+  }
+  
+  function RegisterForm() {
+    return (
+      <React.Fragment>
+        <label for="fullname">full name</label>
+        <input type="text" id="fullname" />
+        <label for="email">email</label>
+        <input type="text" id="email" />
+        <label for="password">password</label>
+        <input type="text" id="password" />
+        <label for="confirmpassword">confirm password</label>
+        <input type="text" id="confirmpassword" />
+        <input type="submit" value="submit" class="submit" />
+      </React.Fragment>
+    );
+  }
+  
+  export default  Signin;
 
