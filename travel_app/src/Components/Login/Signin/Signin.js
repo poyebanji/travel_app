f4cc7df675bd7bd54e8a20a78726dbbc5a1d71f5
Amenzesho <71402496+Amenzesho@users.noreply.github.com>
2020-11-22 21:43:36 -0700

    travel

--- travel_app/src/Components/Login/Signin/Signin.js
+++ travel_app/src/Components/Login/Signin/Signin.js
@@ -3,17 +3,38 @@ import React, { Component } from 'react';
 class Signin extends Component {
   constructor({props}){
     super(props)
-    this.state = {  }
+    this.state = { 
+      signinEmail: '',
+      signinPassword: ''
+     }
+  }
+ 
+  onEmailChange = (event) => {
+    this.setState({signinEmail:event.target.value})
+  
   }
 
-  componentDidMount() {
-    fetch('')
-    .then(Response=> Response.json())
-    .then();
+  onPasswordChange = (event)=> {
+    this.setState({signinPassword:event.target.value})
   }
+
+  // componentDidMount() {
+  //   fetch('')
+  //   .then(Response=> Response.json())
+  //   .then();
+  // }
 
 onSubmitSignin = ()=> {
-  
+  fetch('http://localhost:3000/signin', {
+    method:'POST',
+    headers:{'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      email: this.state.signinEmail,
+      password: this.state.signinPassword
+    })
+  })
+  .then (response => response.json() )
+  .then (user => console.log(user))
   this.props.onRouteChange('home')
 }
 
@@ -22,9 +43,9 @@ onSubmitSignin = ()=> {
     return (
       <React.Fragment>
       <label htmlFor="username">USERNAME</label>
-      <input type="text" id="username" />
-      <label htmlFor="password">PASSWORD</label>
-      <input type="text" id="password" />
+      <input type="text" id="username" onChange={this.onEmailChange} />
+      <label htmlFor="password" >PASSWORD</label>
+      <input type="text" id="password"  onChange={this.onPasswordChange}/>
       <input  onClick = {this.onSubmitSignin} type="submit" value="submit" className="submit" />
       <div>Forgot your password</div>
     </React.Fragment>
