08865f0aa9046d43362081b1fa6c888d0b1572d0
MGhebreslassie <messeleghebreslassie@gmail.com>
2020-11-22 22:04:24 -0700

    merge

--- travel_app/src/Components/Question/Question.js
+++ travel_app/src/Components/Question/Question.js
@@ -1,25 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-=======
-
-const request = "loading";
-
-const matchCity = () =>{
-
-    let city = fetch('/questions', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: {
-            personality: "Adventurous"
-        }
-    }).then(respsonse => respsonse.json())
-    .then(data = (data) =>{
-        request = data;
-    })
-}
->>>>>>> cbc85b50ff047fe2665f7a899672e80bacd6231d
 
 let request;
 
@@ -62,7 +41,7 @@ const Question = () => {
         <React.Fragment>
         <div id="main-question-area" className="container-md personality-area">
             <h3 className="personality-heading">What kind of personality do you have?</h3>
-            <form onSubmit={} id="question-form" className="personality-selectors">
+            <form id="question-form" className="personality-selectors">
                 {usersPersonalites}
                 <input type="submit"/>
                
