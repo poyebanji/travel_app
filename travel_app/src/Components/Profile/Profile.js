c69403cfdba53f8bdaef1a7ba8792e81f79e9105
Peter Oyebanji <peter@prob.ca>
2020-12-07 17:22:56 -0700

    profile

--- travel_app/src/Components/Profile/Profile.js
+++ travel_app/src/Components/Profile/Profile.js
@@ -3,16 +3,23 @@ import React from 'react';
 const Profile = ({user}) => {
     return (
         <div>
-            <div>
+            <div className='center '>
+            <div className='inline'>
             <label> Name:</label>
             <div>{user.name}</div>
+            </div>
+            <div className='inline'>
             <label> email:</label>
             <div>{user.email}</div>
-            <label> entries:</label>
+            </div>
+            <div className='inline'>
+            <label> Booked:</label>
             <div>{user.entries}</div>
+            </div>
+            <div className='inline'>
             <label> Date Joined:</label>
             <div>{user.joined}</div>
-
+            </div>
             </div>      
  
         </div>
